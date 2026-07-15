938de4dfb6a5323f2adc304249ae0e4631fb421f
@psamip55 <psamip55@gmail.com>
2026-07-15 13:34:54 +0545

    Template to create functions automatically

--- Lab/Lab-8/template.cpp
+++ Lab/Lab-8/template.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 using namespace std;
 
-template<class T>
+template<class T>//T is generic/template datatype
 
-T square(T num){
-    return num*num;
+T square(T num){//num generic variable
+    return num*num;//template function
+}
+template <class t1, class t2>
+t1 add(t1 num1, t2 num2){
+    return num1+num2;
 }
 
 int main(){
     cout<<square<int>(3)<<endl;
     cout<<square<float>(4.4)<<endl;;
-    cout<<square<char>('a');
+    cout<<square<char>('a')<<endl;
+    cout<<add<int,int>(2,3)<<endl;
+    cout<<add<int,float>(2,2.5)<<endl;
+    cout<<add<char,int>('a',1)<<endl;
 
     return 0;
 }
